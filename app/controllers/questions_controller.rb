02d12ae01ce038ad8eb0235ab679a53f3e336712
Mike Moore <mikemoore1618@gmail.com>
2018-06-07 09:09:10 -0700

    Completed CRUD functionality

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,17 +10,32 @@ class QuestionsController < ApplicationController
   end
 
   def new
+      @question= Question.new
   end
 
   def create
+    @question = Question.new
+    @question.body = params[:question][:body]
+    @question.answered = params[:question][:answered]
+    @question.save
+    redirect_to '/questions'
   end
 
   def edit
+    @question = Question.find(params[:id])
   end
 
   def update
+    @question = Question.find(params[:id])
+    @question.body = params[:question][:body]
+    @question.answered = params[:question][:answered]
+    @question.save
+    redirect_to '/questions'
   end
 
   def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to '/questions'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
     root 'questions#index'
     get '/questions' => 'questions#index'
-    get '/questions/:id' => 'questions#show' as :qoute
-   
+    get '/questions/new' => 'questions#new' 
+    get '/questions/:id' => 'questions#show', as: :question
+    post '/questions' => 'questions#create'
+    get 'questions/:id/edit' => 'questions#edit', as: :edit_question
+    patch 'questions/:id' => 'questions#update'
+    delete 'questions/:id' => 'questions#destroy'
 end
